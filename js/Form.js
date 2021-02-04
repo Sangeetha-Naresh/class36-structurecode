b64a402832ebabcf420ed23b30ae5c56dbf80f83
Sangeetha-Naresh <73550579+Sangeetha-Naresh@users.noreply.github.com>
2021-02-04 12:48:20 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -10,7 +10,7 @@ class Form
       
       var input = createInput("     ");
       var button = createButton('      ');
-      var greeting = createElement('    ');
+     
       
       input.position(    ,   );
       button.position(   ,   );
